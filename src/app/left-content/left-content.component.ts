b27bf8a7578b0a4da00a87a40a4bd251ed3aa2cc
Jérémy Martin <jeremymartinsio1@gmail.com>
2024-09-18 12:38:17 +0800

    init

---
+++ src/app/left-content/left-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-content.component.html',
+  styleUrl: './left-content.component.scss'
+})
+export class LeftContentComponent {
+
+}
---
+++ src/app/right-content/right-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './right-content.component.html',
+  styleUrl: './right-content.component.scss'
+})
+export class RightContentComponent {
+
+}
